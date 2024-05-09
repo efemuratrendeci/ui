7d89c4d5bac2a2eebb81bac4a35afdbdc09cbea6
Efe Rendeci <efemuratrendeci@hotmail.com>
2024-05-09 19:57:20 +0300

    feat: draft 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,10 @@
     </p>
   </div>
 </div>
-<h2 class="tech_stack_title">Timeline</h2>
+<h2 class="title">Timeline</h2>
 <br />
 <div class="timeline_container">
-  <h3>2017</h3>
+  <h3>Before</h3>
   <div class="gap"></div>
   <div>
     <img src="yeo.jpg" alt="" class="rounded_img company_icon img_contain" />
@@ -30,6 +30,11 @@
   </div>
   <div class="gap"></div>
   <div>
+    <img src="iu.png" alt="" class="rounded_img company_icon img_contain" />
+    <p>2017</p>
+  </div>
+  <div class="gap"></div>
+  <div>
     <img src="nebim.webp" alt="" class="rounded_img company_icon img_contain" />
     <p>2018</p>
   </div>
@@ -39,11 +44,11 @@
     <p>2022</p>
   </div>
   <div class="gap"></div>
-  <h3>2024</h3>
+  <h3>Today</h3>
 </div>
 
 <div class="tech_stack_container">
-  <h2 class="tech_stack_title">Tech Stack</h2>
+  <h2 class="title">Tech Stack</h2>
   <p class="tech_stack_list_title">Languages</p>
   <div class="tech_stack_list">
     <div class="tech_stack_list_item">
@@ -168,5 +173,236 @@
         <img src="react.png" alt="" class="rounded_img tech_icon" />
       </div>
     </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Entity Framework</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="efcore.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Next.js</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="nextjs.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Nest.js</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="nestjs.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
   </div>
+  <div class="tech_stack_list">
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Karate Framework</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="karate.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Selenium</p>
+          <div class="status-bar">
+            <div class="x2"></div>
+          </div>
+        </div>
+        <img src="selenium.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>.Net Core</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="netcore.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Vue.js</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="vuejs.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Material Icon</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="mui.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Bootstrap</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="bootstrap.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+  </div>
+  <br />
+  <p class="tech_stack_list_title">Databases</p>
+  <br />
+  <div class="tech_stack_list">
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Microsoft SQL</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="sql-server.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Postgre SQL</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="postgre.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>MySQL</p>
+          <div class="status-bar">
+            <div class="x4"></div>
+          </div>
+        </div>
+        <img src="mysql.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Apache Druid</p>
+          <div class="status-bar">
+            <div class="x4"></div>
+          </div>
+        </div>
+        <img src="druid.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Redis</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="redis.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+  </div>
+  <p class="tech_stack_list_title">Others</p>
+  <div class="tech_stack_list">
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Docker</p>
+          <div class="status-bar">
+            <div class="x4"></div>
+          </div>
+        </div>
+        <img src="docker.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Github</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="github.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Apache Kafka</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="kafka.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Rabbit MQ</p>
+          <div class="status-bar">
+            <div class="x1"></div>
+          </div>
+        </div>
+        <img src="rabbitmq.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Selenium Grid</p>
+          <div class="status-bar">
+            <div class="x2"></div>
+          </div>
+        </div>
+        <img src="selenium_grid.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Superset</p>
+          <div class="status-bar">
+            <div class="x4"></div>
+          </div>
+        </div>
+        <img src="superset.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+  </div>
+  <br />
 </div>

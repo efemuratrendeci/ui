0393d759bf2766203f6e680bb4b1ccdc35a67b4e
Efe Rendeci <efemuratrendeci@hotmail.com>
2024-05-09 18:43:35 +0300

    feat: draft

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import '../styles/global.scss';
+</script>
+
+<section:head>
+    <title>Efe Murat Rendeci</title>
+    <link rel="icon" type="image/png" href="/favicon.png">
+    <meta name="description" content="Portfolio site for Efe Murat Rendeci">
+</section:head>
+
+<div>
+    <slot/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,172 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="main_information_container">
+  <img src="me.jpg" alt="" class="rounded_img profile_photo" />
+  <div class="main_information_container__body">
+    <div class="name">
+      <p>Welcome. I am <strong>Efe Murat Rendeci</strong></p>
+    </div>
+    <p class="short_info">
+      I am Full Stack Developer.<br />
+      My current interests on game development with Unity.
+    </p>
+  </div>
+</div>
+<h2 class="tech_stack_title">Timeline</h2>
+<br />
+<div class="timeline_container">
+  <h3>2017</h3>
+  <div class="gap"></div>
+  <div>
+    <img src="yeo.jpg" alt="" class="rounded_img company_icon img_contain" />
+    <p>2016</p>
+  </div>
+  <div class="gap"></div>
+  <div>
+    <img
+      src="aselsan.jpeg"
+      alt=""
+      class="rounded_img company_icon img_contain"
+    />
+    <p>2017</p>
+  </div>
+  <div class="gap"></div>
+  <div>
+    <img src="nebim.webp" alt="" class="rounded_img company_icon img_contain" />
+    <p>2018</p>
+  </div>
+  <div class="gap"></div>
+  <div>
+    <img src="mercedes-benz.jpeg" alt="" class="rounded_img company_icon" />
+    <p>2022</p>
+  </div>
+  <div class="gap"></div>
+  <h3>2024</h3>
+</div>
+
+<div class="tech_stack_container">
+  <h2 class="tech_stack_title">Tech Stack</h2>
+  <p class="tech_stack_list_title">Languages</p>
+  <div class="tech_stack_list">
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Javascript</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="js.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Typescript</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="typescript.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>C#</p>
+          <div class="status-bar">
+            <div class="x4"></div>
+          </div>
+        </div>
+        <img src="c-sharp.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Java</p>
+          <div class="status-bar">
+            <div class="x1"></div>
+          </div>
+        </div>
+        <img src="java.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Dart</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="dart.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Golang</p>
+          <div class="status-bar">
+            <div class="x2"></div>
+          </div>
+        </div>
+        <img
+          src="golang.png"
+          alt=""
+          class="rounded_img tech_icon img_contain"
+        />
+      </div>
+    </div>
+  </div>
+  <br />
+  <p class="tech_stack_list_title">Environments</p>
+  <div class="tech_stack_list">
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Node.js</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="node.webp" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+  </div>
+  <br />
+  <p class="tech_stack_list_title">Frameworks</p>
+  <div class="tech_stack_list">
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Express.js</p>
+          <div class="status-bar">
+            <div class="x5"></div>
+          </div>
+        </div>
+        <img src="express.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>Flutter</p>
+          <div class="status-bar">
+            <div class="x3"></div>
+          </div>
+        </div>
+        <img src="flutter.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+    <div class="tech_stack_list_item">
+      <div class="tooltip">
+        <div class="tooltip_context">
+          <p>React.js</p>
+          <div class="status-bar">
+            <div class="x4"></div>
+          </div>
+        </div>
+        <img src="react.png" alt="" class="rounded_img tech_icon" />
+      </div>
+    </div>
+  </div>
+</div>

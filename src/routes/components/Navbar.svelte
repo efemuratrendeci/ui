e5cbe0ccfba2d27488d20a59d86245dbf729859c
Efe Rendeci <efemuratrendeci@hotmail.com>
2024-05-13 18:23:49 +0300

    feat: nav changed

--- src/routes/components/Navbar.svelte
+++ src/routes/components/Navbar.svelte
@@ -1,12 +1,3 @@
-<script>
-  let buttonSign = "+";
-  const handleActionButtonClick = () => {
-    const navbarItems = document.querySelector(".navbar_items");
-    navbarItems.classList.toggle("visible");
-    buttonSign = buttonSign === "+" ? "-" : "+";
-  };
-</script>
-
 <div class="navbar">
   <div class="navbar_items">
     <a href="https://www.linkedin.com/in/efe-murat-rendeci/"
@@ -21,12 +12,7 @@
     <a href="https://www.instagram.com/efemuratrendeci/"
       ><img src="instagram.png" alt="" class="nav_icon" /></a
     >
-    <div>
-      <img src="location.png" alt="" class="nav_icon" />
-      <div class="tooltip">
-        <p class="tooltip_context">Istanbul/Turkiye</p>
-      </div>
-    </div>
+    <img src="location.png" alt="" class="nav_icon" />
+    <p>Istanbul/Turkiye</p>
   </div>
-  <button on:click={handleActionButtonClick} class="navbar_button">{buttonSign}</button>
 </div>
